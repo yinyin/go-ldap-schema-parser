ba994808ae743fd90cbe8d6cb150cfeb25914e35
Yinyin L <yinyinl@gmail.com>
2019-04-21 01:21:51 +0800

    update type ID

--- dev-tool/keyword-type-lookup-table-gen/main.go
+++ dev-tool/keyword-type-lookup-table-gen/main.go
@@ -13,8 +13,8 @@ import (
 )
 
 var keywordTypeIdentifierMapping = map[string]string{
-	"OIDs":    "OIDS_ATTR_KEYWORD",
-	"RuleIDS": "OIDS_ATTR_KEYWORD",
+	"OIDs":    "NOIDS_ATTR_KEYWORD",
+	"RuleIDS": "NOIDS_ATTR_KEYWORD",
 }
 
 const mappingTrap = "\\s*\"([A-Z]+)\"[`\\s\\\\s+]+\\*\\*([A-Za-z]+)\\*\\*\\s*"
